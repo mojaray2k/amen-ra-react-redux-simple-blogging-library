54b94015ef51ff06eefacdf57fed3c1337a7731c
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-07-19 09:39:18 -0400

    addding initial code for app

---
+++ src/components/posts_new.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostsNew extends Component {
+  
+  renderField(field) {
+    const { meta: { touched, error}, label } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input 
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>        
+      </div>
+    )
+  }
+
+  onSubmit(values) {    
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render(){
+    const { handleSubmit } = this.props;
+
+      return (
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+            <Field 
+              label="Title"
+              name="title"
+              component={this.renderField}
+            />
+             <Field 
+              label="Categories"
+              name="categories"
+              component={this.renderField}
+            /> 
+            <Field 
+              label="Post Content"
+              name="content"
+              component={this.renderField}
+            /> 
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <Link className="btn btn-danger" to="/">
+              Cancel
+            </Link>
+          </form>
+      );
+  }
+}
+
+function validate(values){
+  const errors = {};
+
+  // Validate the input from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+  // if errors is empty, the form is fine to submit
+  // if errros has any properties, redux form assumes the form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(
+  connect(null, { createPost })(PostsNew)
+);
+
---
+++ src/components/posts_new.md
@@ -0,0 +1,9 @@
+# Redux Form and the Posts New Component
+
+![posts_new](posts_new.png)
+
+1. Scaffold `Posts/New` component
+2. add Route Congifuration
+3. Add navigation between Index and New
+4. dd form to `Posts/New`
+5. Make action creator to save post
